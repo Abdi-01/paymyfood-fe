27ae545e6c5ecc4b5467ec41c87d3b3927af49cf
marxtinn <butarbutar.m@gmail.com>
2023-03-01 11:16:27 +0700

    add component checkout card and total

--- src/Components/CheckoutCard.jsx
+++ src/Components/CheckoutCard.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import React from "react";
+import {
+    Card,
+    CardBody,
+    Button,
+    Text,
+    Image,
+    Stack,
+    Heading,
+    Box,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 function CheckoutCard() {
-  return (
-    <div>CheckoutCard</div>
-  )
+    const [count, setCount] = useState(0);
+    const [disabled, setDisabled] = useState(false);
+
+    const btnIncrement = () => {
+        setCount(count + 1);
+        setDisabled(false);
+    };
+
+    const btnDecrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        } else {
+            setDisabled(true);
+        }
+    };
+    return (
+        <div>
+            <Card
+                direction={"row"}
+                overflow="hidden"
+                variant="outline"
+                maxH="32"
+                mx={"4"}
+                bgColor="#393E46"
+                color={"#EEEEEE"}
+            >
+                <Image
+                    objectFit="cover"
+                    maxW={"32"}
+                    src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+                    alt="Caffe Latte"
+                />
+
+                <Stack>
+                    <CardBody>
+                        <Box>
+                            <Text size="xs" fontWeight={"semibold"}>
+                                Spaghetti Carbonara
+                            </Text>
+                            <Text py="1" fontSize={"xs"} textAlign="left">
+                                Rp. 15.000,00-
+                            </Text>
+                        </Box>
+                        <Box
+                            display={"flex"}
+                            justifyContent={"right"}
+                            alignItems="center"
+                        >
+                            <Button
+                                variant="link"
+                                bgColor={"#00adb5"}
+                                color="#EEEEEE"
+                                _hover={""}
+                                h={"10px"}
+                                w={"10px"}
+                                p={"10px"}
+                                mr="2"
+                                fontSize="lg"
+                                fontWeight={"bold"}
+                                onClick={btnDecrement}
+                                disabled={disabled}
+                            >
+                                -
+                            </Button>
+                            <Box my={"auto"}>
+                                <Text my={"auto"}>{count}</Text>
+                            </Box>
+                            <Button
+                                variant="link"
+                                bgColor={"#00adb5"}
+                                color="#EEEEEE"
+                                _hover={""}
+                                h={"10px"}
+                                w={"10px"}
+                                py="10px"
+                                ml="2"
+                                fontSize="lg"
+                                fontWeight={"bold"}
+                                onClick={btnIncrement}
+                            >
+                                +
+                            </Button>
+                        </Box>
+                    </CardBody>
+                </Stack>
+            </Card>
+        </div>
+    );
 }
 
-export default CheckoutCard
+export default CheckoutCard;
--- src/Components/CheckoutTotal.jsx
+++ src/Components/CheckoutTotal.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Table, Tbody, Tr, Td, Divider, Tfoot } from "@chakra-ui/react";
 
 function CheckoutTotal() {
-  return (
-    <div>CheckoutTotal</div>
-  )
+    return (
+        <Table variant="simple" width={"full"}>
+            <Tbody>
+                <Tr>
+                    <Td>Sub-Total</Td>
+                    <Td isNumeric>25.4</Td>
+                </Tr>
+                <Tr>
+                    <Td>Discount</Td>
+                    <Td isNumeric>25.4</Td>
+                </Tr>
+                <Tr>
+                    <Td>Tax (11%)</Td>
+                    <Td isNumeric>30.48</Td>
+                </Tr>
+            </Tbody>
+            <Tfoot
+                borderWidth={"medium"}
+                borderTop={"4px solid #222831"}
+                mt="2"
+            >
+                <Tr>
+                    <Td>TOTAL</Td>
+                    <Td isNumeric>0.91444</Td>
+                </Tr>
+            </Tfoot>
+        </Table>
+    );
 }
 
-export default CheckoutTotal
+export default CheckoutTotal;
