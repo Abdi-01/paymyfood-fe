5b0638a2e472ee86f7ddc02580b00a022101eaea
danielreinhard1129 <danielreinhard1129@gmail.com>
2023-03-02 10:21:12 +0700

    pmf-3 , pmf-10, pmf-14

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -10,20 +10,21 @@ import {
     CardFooter,
     Button,
 } from "@chakra-ui/react";
+import { API_URL } from "../helper";
 
 function ProductCard(props) {
     return (
         <Card maxW="sm" bgColor={"#393E46"}>
             <CardBody>
                 <Image
-                    src="https://images.unsplash.com/photo-1612874742237-6526221588e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8c3BhZ2hldHRpJTIwY2FyYm9uYXJhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                    src={`${API_URL}${props.image}`}
                     alt="Spaghetti Carbonara"
                     borderRadius="lg"
                     maxH={"32"}
                 />
                 <Stack mt="3" spacing="1">
-                    <Heading size="md" color={"#EEEEEE"}>
-                        {props.produk}
+                    <Heading size="md" color={"#EEEEEE"} textAlign={"center"}>
+                        {props.product}
                     </Heading>
                     <Text color="#00ADB5" fontSize="2xl" fontWeight="bold" textAlign={"center"}>
                         {props.price}
--- src/Components/TabOption.jsx
+++ src/Components/TabOption.jsx
@@ -1,80 +1,55 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel, TabPanels } from "@chakra-ui/react";
 import ProductCard from "./ProductCard";
+import TabPanell from "./TabPanell";
 
-function TabOption() {
+function TabOption(props) {
+
+    const printTabList = () => {
+        return props.dataAllCategory.map((val,idx) => {
+            return <Tab bgColor={"#00ADB5"} color="#EEEEEE" mr={"2"}>
+            {val.category}
+        </Tab>
+        })
+    }
+
+    const printTabPanel = () => {
+        return props.dataAllCategory.map((val,idx) => {
+            return <TabPanell />
+        })
+    }
+
+    const printTabPanelAllProducts = () => {
+        return props.dataAllProducts.map((val,idx) => {
+            return <ProductCard product={val.product} price={val.price} image={val.image}/>
+        })
+    }
+
+    console.log("props get all product taboption : ", props.dataAllProducts)
+
+
     return (
         <Tabs variant="soft-rounded">
             <TabList>
                 <Tab bgColor={"#00ADB5"} color="#EEEEEE" mr={"2"}>
                     All Products
                 </Tab>
-                <Tab bgColor={"#00ADB5"} color="#EEEEEE" mr={"2"}>
-                    Main Course
-                </Tab>
-                <Tab bgColor={"#00ADB5"} color="#EEEEEE" mr={"2"}>
-                    Appetizers
-                </Tab>
-                <Tab bgColor={"#00ADB5"} color="#EEEEEE" mr={"2"}>
-                    Beverages
-                </Tab>
-                <Tab bgColor={"#00ADB5"} color="#EEEEEE" mr={"2"}>
-                    Desserts
-                </Tab>
+                {printTabList()}
             </TabList>
             <TabPanels ml={"-4"}>
+                {/* TAB-PANEL 1 */}
                 <TabPanel
                     display={"flex"}
                     flexWrap={"wrap"}
                     color={"#EEEEEE"}
                     gap={"2"}
                 >
-                    <ProductCard />
+                    {printTabPanelAllProducts()}
                 </TabPanel>
-                <TabPanel
-                    display={"flex"}
-                    flexWrap={"wrap"}
-                    color={"#EEEEEE"}
-                    gap={"2"}
-                >
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                </TabPanel>
-                <TabPanel
-                    display={"flex"}
-                    flexWrap={"wrap"}
-                    color={"#EEEEEE"}
-                    gap={"2"}
-                >
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                </TabPanel>
-                <TabPanel
-                    display={"flex"}
-                    flexWrap={"wrap"}
-                    color={"#EEEEEE"}
-                    gap={"2"}
-                >
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                </TabPanel>
-                <TabPanel
-                    display={"flex"}
-                    flexWrap={"wrap"}
-                    color={"#EEEEEE"}
-                    gap={"2"}
-                >
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                </TabPanel>
+
+
+                {printTabPanel()}
+
             </TabPanels>
         </Tabs>
     );
